ba9cd0ef2c01b46d6b0c259f6fc00dc8c5aabdad
Ramkumar Kaleeswaran <ramku.user@gmail.com>
2024-09-25 17:33:12 +0530

    second commit

--- demo-test.tf
+++ demo-test.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region     = "ap-south-1"
-  access_key = var.sandbox_access_key 
-  secret_key = var.sandbox_secret_key 
+  access_key = var.info1 
+  secret_key = var.info2 
 } 
 
 
